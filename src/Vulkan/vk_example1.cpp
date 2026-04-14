7b8f16f8a638216ec8dc5b52173adb1141ec755b
DM8AT <DM8AT@users.noreply.github.com>
2026-04-14 18:50:56 +0200

    re-worked the main loading system to support different examples

---
+++ src/Vulkan/vk_example1.cpp
@@ -0,0 +1,21 @@
+/**
+ * @file vk_example1.cpp
+ * @author DM8AT
+ * @brief define the first Vulkan example
+ * @version 0.1
+ * @date 2026-04-14
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+//add the example
+#include "vk_example1.h"
+
+//add GLGE
+#define GLGE_NO_THANKS_MSG
+#include "GLGE.h"
+
+void vk_example1() {
+    //initialize GLGE
+    GLGE::Instance::init();
+}
