a30e9ce9c734183a7401e74ebdb7ae51d688f096
DM8AT <DM8AT@users.noreply.github.com>
2026-05-17 21:12:55 +0200

    fixed bugs in the new example launcher

--- src/main.cpp
+++ src/main.cpp
@@ -17,31 +17,31 @@
 /**
  * @brief define if the configuration is changed when an example fails or if the failure is passed
  */
-constexpr bool RETRY = false;
+constexpr bool RETRY = true;
 
 /**
  * @brief define a function pointer to a graphic backend loader function
  */
-typedef GLGE::Graphic::Backend::Graphic::Description* (*PFN_GraphicBackendLoader)();
+typedef std::unique_ptr<GLGE::Graphic::Backend::Graphic::Description> (*PFN_GraphicBackendLoader)();
 
 /**
  * @brief store all graphic backend
  */
 std::vector<std::pair<const char*, PFN_GraphicBackendLoader>> GRAPHIC_BACKEND_MAP = {
-    std::pair("OpenGL", []() -> GLGE::Graphic::Backend::Graphic::Description* {return new GLGE::Graphic::Builtin::Graphics::OpenGL();}),
-    std::pair("Vulkan", []() -> GLGE::Graphic::Backend::Graphic::Description* {return new GLGE::Graphic::Builtin::Graphics::Vulkan();})
+    std::pair("OpenGL", []() -> std::unique_ptr<GLGE::Graphic::Backend::Graphic::Description> {return std::make_unique<GLGE::Graphic::Builtin::Graphics::OpenGL>();}),
+    std::pair("Vulkan", []() -> std::unique_ptr<GLGE::Graphic::Backend::Graphic::Description> {return std::make_unique<GLGE::Graphic::Builtin::Graphics::Vulkan>();})
 };
 
 /**
  * @brief define a function pointer to a video backend loader function
  */
-typedef GLGE::Graphic::Backend::Video::Description* (*PFN_VideoBackendLoader)();
+typedef std::unique_ptr<GLGE::Graphic::Backend::Video::Description> (*PFN_VideoBackendLoader)();
 
 /**
  * @brief store all graphic backend
  */
 std::vector<std::pair<const char*, PFN_VideoBackendLoader>> VIDEO_BACKEND_MAP = {
-    std::pair("SDL3", []() -> GLGE::Graphic::Backend::Video::Description* {return new GLGE::Graphic::Builtin::Video::SDL3();})
+    std::pair("SDL3", []() -> std::unique_ptr<GLGE::Graphic::Backend::Video::Description> {return std::make_unique<GLGE::Graphic::Builtin::Video::SDL3>();})
 };
 
 /**
@@ -59,7 +59,7 @@ static std::string structureMapToString(const std::vector<std::pair<const char*,
     //keep track of the current element ID
     size_t id = 0;
     //iterate over all elements
-    for (const auto& [name, ptr] : list) {
+    for (const auto& [name, _] : list) {
         //create the beginning section of the element
         std::string begin("[");
         begin += std::to_string(id);
@@ -73,6 +73,14 @@ static std::string structureMapToString(const std::vector<std::pair<const char*,
 }
 
 int main() {
+    //sanity check that all lists are filled
+    if (EXAMPLES.size() == 0)
+    {throw GLGE::Exception("Failed to run the example launcher - the example list is empty", "main");}
+    if (GRAPHIC_BACKEND_MAP.size() == 0)
+    {throw GLGE::Exception("Failed to run the example launcher - the graphic backend list is empty", "main");}
+    if (VIDEO_BACKEND_MAP.size() == 0)
+    {throw GLGE::Exception("Failed to run the example launcher - the video backend list is empty", "main");}
+
     //print all loaded examples
     std::cout << "Loaded examples:\n";
     std::cout << structureMapToString(EXAMPLES);
@@ -80,7 +88,7 @@ int main() {
     //get the example to run
     size_t exampleId = UINT64_MAX;
     std::cin >> exampleId;
-    if (exampleId > EXAMPLES.size())
+    if (exampleId >= EXAMPLES.size())
     {throw GLGE::Exception("The selected example does not map to a valid example ID", "main");}
 
     //print all loaded graphic backends
@@ -89,7 +97,7 @@ int main() {
     //get the graphic API to use
     size_t graphicApiId = UINT64_MAX;
     std::cin >> graphicApiId;
-    if (graphicApiId > GRAPHIC_BACKEND_MAP.size())
+    if (graphicApiId >= GRAPHIC_BACKEND_MAP.size())
     {throw GLGE::Exception("The selected graphic API ID does not map to a valid graphic API", "main");}
 
     //print all loaded video backends
@@ -98,31 +106,36 @@ int main() {
     //get the video api to use
     size_t videoApiId = UINT64_MAX;
     std::cin >> videoApiId;
-    if (videoApiId > VIDEO_BACKEND_MAP.size())
+    if (videoApiId >= VIDEO_BACKEND_MAP.size())
     {throw GLGE::Exception("The selected video API ID does not map to a valid video API", "main");}
 
     //setup for potential looping
     bool success = false;
-    size_t initalGraphicAPI = graphicApiId;
+    size_t initialGraphicAPI = graphicApiId;
     size_t initialVideoAPI = videoApiId;
     //helper
     auto stepHelper = [&]() {
         //re-trying in a new configuration.
         ++graphicApiId;
-        if (graphicApiId > GRAPHIC_BACKEND_MAP.size()) 
+        if (graphicApiId >= GRAPHIC_BACKEND_MAP.size()) 
         {graphicApiId = 0;}
-        if (graphicApiId == initalGraphicAPI)
+        if (graphicApiId == initialGraphicAPI)
         {++videoApiId;}
-        if (videoApiId > VIDEO_BACKEND_MAP.size()) 
+        if (videoApiId >= VIDEO_BACKEND_MAP.size()) 
         {videoApiId = 0;}
-        if (videoApiId == initialVideoAPI)
+        if (videoApiId == initialVideoAPI && graphicApiId == initialGraphicAPI)
         {throw GLGE::Exception("Failed to find a configuration that can run the selected example", "main");}
     };
     //if `RETRY` is enabled try to loop when the example crashes
     while (!success) {
         try {
+            //create the backend descriptors
+            auto gDescr = ((*(GRAPHIC_BACKEND_MAP[graphicApiId].second))());
+            auto vDescr = (*(VIDEO_BACKEND_MAP[videoApiId].second))();
+
             //create the APIs and run the code, storing the return value
-            unsigned char ret = (*(EXAMPLES[exampleId].second))((*(GRAPHIC_BACKEND_MAP[graphicApiId].second))(), (*(VIDEO_BACKEND_MAP[videoApiId].second))());
+            unsigned char ret = (*(EXAMPLES[exampleId].second))(gDescr.get(), vDescr.get());
+
             //validate success
             if (ret == 0 || !RETRY) {
                 success = true;
@@ -133,7 +146,7 @@ int main() {
                 //step
                 stepHelper();
             }
-        } catch (std::exception exception) {
+        } catch (const std::exception& exception) {
             if (RETRY) {
                 //print it but ignore it
                 std::cout << "[FATAL ERROR] Failed to run the selected example in the current configuration. An exception was thrown. What:\n";
